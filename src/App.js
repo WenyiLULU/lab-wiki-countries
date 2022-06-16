d9c0bb5719b1561f53603d8b51b0d39a8e91b0f6
WenyiLULU <luwenyi1991@gmail.com>
2022-06-16 22:27:34 +0200

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Navbar from './Components/Navbar';
 import CountryDetails from './Components/CountryDetails';
-import CountriesList from './Components/CountriesList'
+import CountriesList from './Components/CountriesList';
+import axios from 'axios';
 
-import countriesJson from './countries.json'
+import countriesJson from './countries.json';
 
 function App() {
 
-  const [countries, setCountries] = useState(countriesJson)
+  const [countries, setCountries] = useState([])
+  const [fetching, setFetching] = useState(true);
+
+  const apiURL = "https://ih-countries-api.herokuapp.com/countries"
+
+  useEffect(()=>{
+    axios.get(apiURL)
+        .then(response => {
+          setCountries(response.data);
+          setFetching(false);
+        })
+  }, [])
 
   return (
     <div className="App">
       
       <Navbar  />
+
+      {fetching && <p>Loading ...</p>}
+      
       <div className="container">
         <div className="row">
           <CountriesList countries={countries} />
